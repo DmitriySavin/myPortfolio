e7b132929042b4f63e1083144590557b97866301
DmitriySavin <debilv556@gmail.com>
2026-04-16 20:53:28 +0300

    add

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,14 @@
-import { Suspense } from "react";
+
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import { Outlet } from "react-router-dom";
+
+
 const Layout = () => {
+
+
   return (
-    <Suspense fallback={"Loading..."}>
+    <>
       <Header />
       <main>
         <div className="container">
@@ -13,7 +17,7 @@ const Layout = () => {
       </main>
 
       <Footer />
-    </Suspense>
+    </>
   );
 };
 
